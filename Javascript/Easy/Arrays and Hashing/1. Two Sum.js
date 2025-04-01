bffb7548c7c88aa8db35b83ccc7317cb24866ae2
Cordeva <ahmed.abdelaaall@gmail.com>
2025-04-01 21:34:21 +0200

    two sum in js

---
+++ Javascript/Easy/Arrays and Hashing/1. Two Sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (diff in map) {
+      return [map[diff], i];
+    } else {
+      map[nums[i]] = i;
+    }
+  }
+  return [];
+};
