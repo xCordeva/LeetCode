42426b0885d2da18681288bb6e690902fe1a0d13
Cordeva <ahmed.abdelaaall@gmail.com>
2025-04-13 14:28:27 +0200

    removed comments

--- Javascript/Medium/Arrays and Hashing/36. Valid Sudoku.js
+++ Javascript/Medium/Arrays and Hashing/36. Valid Sudoku.js
@@ -6,7 +6,7 @@ var isValidSudoku = function (board) {
   let boxSet = Array.from({ length: 9 }, () => new Set());
   let rowSet = Array.from({ length: 9 }, () => new Set());
   let colSet = Array.from({ length: 9 }, () => new Set());
-  // knowing that board.length == 9 & board[i].length == 9 then used 9 instead of board.length to make the time complex O(1)
+
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       let num = board[i][j];
@@ -28,4 +28,3 @@ var isValidSudoku = function (board) {
   }
   return true;
 };
-// this sol is a time complexity of O(1) and a space complexity of O(1)
